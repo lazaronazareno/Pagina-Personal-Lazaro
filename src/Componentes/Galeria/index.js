46cdeed2c97e2eed521051d932e4be955076c961
lazaronazareno <brankocruz15@gmail.com>
2020-01-27 17:59:31 -0300

    enlace exitosamente la carpeta galeria!

---
+++ src/Componentes/Galeria/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Gallery extends React.Component {
+    render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-logo" alt="logo">Mireme convertirme en una galeria!!!!!</h1> 
+          <div>
+              <h1>Galeria</h1>
+
+          </div>      
+        </header>
+      </div>
+    );
+  }
+  }
+  
+  export default Gallery;
---
+++ src/Componentes/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Gallery from './Galeria/index'
+
+class Inicio extends React.Component {
+    render(){
+    return (
+      <div className="App">
+        <header className="App-header">       
+      <Gallery/>
+        </header>
+      </div>
+    );
+  }
+  }
+  
+  export default Inicio;
